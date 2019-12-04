8f05903f9bf72b40bd088799dc1f06beec82ed3d
Elena Piaggio <elenapiaggio@gmail.com>
2019-12-04 21:43:01 +0100

    ADD update component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import FetchExample from './components/FetchExample';
+import Update from './life-cycle/Update';
 
 function App() {
   return (
     <div className="App">
-      <FetchExample/>
+      {/* <FetchExample/> */}
+      <Update />
     </div>
   );
 }
---
+++ src/life-cycle/Update.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+const ANIMALS = {
+    cat: 'https://goo.gl/PoQQXb',
+    dolphin: 'https://goo.gl/BbiKCd',
+    panda: 'https://goo.gl/oNbtoq'
+}
+class Update extends Component {
+    
+    render() {
+        return (
+            <div>
+                <h4>ComponentWillReceiveProps</h4>
+            </div>
+        );
+    }
+}
+
+export default Update;
