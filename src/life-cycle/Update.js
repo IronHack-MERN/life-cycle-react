9e8d4b8bec7b820038e8ae2e742a07aa81e29b9b
Elena Piaggio <elenapiaggio@gmail.com>
2019-12-05 14:28:05 +0100

    ADD method _renderAnimalButton

--- src/life-cycle/Update.js
+++ src/life-cycle/Update.js
@@ -2,31 +2,31 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 const ANIMALS_IMG = {
-    cat: 'https://goo.gl/PoQQXb',
-    dolphin: 'https://goo.gl/BbiKCd',
-    panda: 'https://goo.gl/oNbtoq'
+  cat: 'https://goo.gl/PoQQXb',
+  dolphin: 'https://goo.gl/BbiKCd',
+  panda: 'https://goo.gl/oNbtoq'
 }
 
 class AnimalImage extends Component {
-    state = { src: ANIMALS_IMG[this.props.animal] }
+  state = { src: ANIMALS_IMG[this.props.animal] }
 
-    componentWillReceiveProps (nextProps){ 
-      console.log('componentWillReceiveProps')
-      console.log(nextProps)
-      this.setState({ src: ANIMALS_IMG[nextProps.animal] })
-    }
+  componentWillReceiveProps (nextProps){ 
+    console.log('componentWillReceiveProps')
+    console.log(nextProps)
+    this.setState({ src: ANIMALS_IMG[nextProps.animal] })
+  }
 
-    render() {
-      return(
-        <div>
-          <p>Selected {this.props.animal}</p>
-          <img
-            alt={this.props.animal}
-            src={this.state.src}
-            width='250' />
-        </div>
-      )
-    }
+  render() {
+    return(
+      <div>
+        <p>Selected {this.props.animal}</p>
+        <img
+          alt={this.props.animal}
+          src={this.state.src}
+          width='250' />
+      </div>
+    )
+  }
 }
 
 AnimalImage.propTypes = {
@@ -35,25 +35,27 @@ AnimalImage.propTypes = {
 
 class Update extends Component {
     state = { animal: 'panda' }
+
+    _renderAnimalButton(animal){
+      return(
+        <button onClick = { () => this.setState({ animal }) }>
+          {animal}
+        </button>
+      )
+    }
+
     render() {
-        return (
-            <div>
-                <h4>ComponentWillReceiveProps</h4>
-                <button onClick = { () => this.setState({ animal: 'panda' }) }>
-                  Panda
-                </button>
-                <button onClick = { () => this.setState({ animal: 'cat' }) }>
-                  Cat
-                </button>
-                <button onClick = { () => this.setState({ animal: 'dolphin' }) }>
-                  Dolphin
-                </button>
-
-
-                <AnimalImage
-                  animal = {this.state.animal}
-                />
-            </div>
+      return (
+        <div>
+          <h4>ComponentWillReceiveProps</h4>
+          {this._renderAnimalButton('panda')}
+          {this._renderAnimalButton('cat')}
+          {this._renderAnimalButton('dolphin')}
+          
+          <AnimalImage
+            animal = {this.state.animal}
+          />
+        </div>
         );
     }
 }
