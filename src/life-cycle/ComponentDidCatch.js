ed567d336ea49e9d44fdc9b794346b4ce5698afc
Elena Piaggio <elenapiaggio@gmail.com>
2019-12-06 20:50:39 +0100

    componentDidCatch done

--- src/life-cycle/ComponentDidCatch.js
+++ src/life-cycle/ComponentDidCatch.js
@@ -1,13 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import ErrorButton from "../components/ErrorThrowingButton";
 
 class ComponentDidCatch extends Component {
-    render() {
-        return (
-            <div>
-               <h4>Ciclo de montaje: ComponentDidCatch</h4> 
-            </div>
-        );
+  state = {
+    hasError: false,
+    errorMsg: ''
+  }
+
+  componentDidCatch(error, errorInfo) {
+    console.log("componentDidCatch")
+    console.log({ error, errorInfo })
+    this.setState({ 
+      hasError: true, 
+      erroMsg: error.toString 
+    })
+  }
+
+  render() {
+    if(this.state.hasError){
+      return (
+        <div>
+          <h4>Error in component: {this.state.errorMsg}</h4>
+          <button onClick={() => { this.setState({ hasError: false })}}>
+            Return to app
+          </button>
+        </div>
+      )
     }
+
+    return(
+      <div>
+          <h4>Ciclo de montaje: ComponentDidCatch</h4>
+          <ErrorButton />
+        </div>
+    )
+  }
 }
 
 export default ComponentDidCatch;
